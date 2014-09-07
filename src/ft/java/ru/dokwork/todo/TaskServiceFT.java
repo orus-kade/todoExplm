0568ccd6cf6b07643b18b988e81b260ab7696ebc
dokwork <vladimir@dokwork.ru>
2014-09-07 16:11:33 +0400

    Add empty test TaskServiceFT

---
+++ src/ft/java/ru/dokwork/todo/TaskServiceFT.java
@@ -0,0 +1,54 @@
+package ru.dokwork.todo;
+
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaskServiceFT {
+
+    private static final String URL = "http://localhost:8080/tasks/";
+
+    Gson gson;
+
+    @Before
+    public void setUp() throws Exception {
+        gson = new Gson();
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testPatch() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testFind() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+}
